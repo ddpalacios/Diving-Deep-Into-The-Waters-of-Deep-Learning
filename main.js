f84b6adf843b9db85b749fe7e5bfa2fe4dccf2f2
ddpalacios <palaciosdanieldario@gmail.com>
2020-02-09 14:16:20 -0500

    NN implementation with brain.js

--- main.js
+++ main.js
@@ -157,8 +157,6 @@ function create_simulation(list, link){
      
     }
 
-
-
 function setup(num_of_hidden_layers){
   list = []
   links = []
@@ -183,10 +181,35 @@ create_simulation(list, links)
 }
 
 
+class Settings{
+  constructor(){
+    this.units = document.getElementById("units").value
+    this.iterations = document.getElementById("iterations").value
+    this.lr = document.getElementById("lr").value
+    this.activation = document.getElementById("activation").value
+  }
 
-function create_hidden_layers(){
-
-    num_of_hidden_layers = document.getElementById("layers").value
-    setup(num_of_hidden_layers)
 }
+function Train(){
+  var setting = new Settings()
+  // Testing dataset
+  const trainingData = [
+    { input: [0, 0], output: [0] },
+    { input: [0, 1], output: [1] },
+    { input: [1, 0], output: [1] },
+    { input: [1, 1], output: [0] }
+  ];
+  var div = document.getElementById("output")
+    const net = new brain.NeuralNetwork({hiddenLayers: [parseInt(setting.units, 10)],
+                                         activation: setting.activation, 
+                                         iterations: parseInt(setting.iterations),
+                                         learningRate: parseFloat(setting.lr),
+                                         log: true
+                                        
+                                        });
 
+  net.train(trainingData)
+  div.innerHTML = net.run([0,1])
+
+    // setup(num_of_hidden_layers)
+}
