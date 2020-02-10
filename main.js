1e88cbeda7c67a4bc243e4adf31ac096bbfc8026
Daniel <palaciosdanieldario@gmail.com>
2020-02-10 09:40:38 -0600

    implementation for Render, Reset, Train

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//Make the DIV element draggagle:
 class Settings{
   constructor(){
     this.units = document.getElementById("units").value
@@ -7,13 +6,35 @@ class Settings{
     this.activation = document.getElementById("activation").value
   }
 
+
+  get_unit_layers(){
+    var units = this.units.split(",").map(Number)
+    return units
+  }
+
+  initialize(simulation){
+    var unit_layers = this.get_unit_layers()
+    for (var unit_quantity=0;  unit_quantity<unit_layers.length; unit_quantity++){
+        var unit = unit_layers[unit_quantity]
+        simulation.setup(unit)
+    }
+  }
+
 }
 
-
-function Train(){
-  
+function Render(){
   var setting = new Settings()
   var simulation = new Render_NN()
+  setting.initialize(simulation)
+
+}
+function Reset(){
+location.reload()
+}
+function Train(){
+  var setting = new Settings()
+  var units_per_layers = setting.get_unit_layers()
+ 
   // Testing dataset
   const trainingData = [
     { input: [0, 0], output: [0] },
@@ -22,20 +43,18 @@ function Train(){
     { input: [1, 1], output: [0] }
   ];
   
-
-
-  var div = document.getElementById("output")
-    const net = new brain.NeuralNetwork({hiddenLayers: [parseInt(setting.units, 10)],
+    const net = new brain.NeuralNetwork({hiddenLayers:units_per_layers,
                                          activation: setting.activation, 
                                          iterations: parseInt(setting.iterations),
                                          learningRate: parseFloat(setting.lr),
-                                         log: false
+                                         log: true
                                         
                                         });
-  simulation.setup(setting.units)
+  
+  
   document.getElementById("demo").innerHTML = 'Calculating...'
-  net.train(trainingData).learningRatelatin
-  div.innerHTML = net.run([0,1])
+  net.train(trainingData)
+  document.getElementById("output").innerHTML = "Probability result for current weights: " + net.run([0,1])
 
     
 
